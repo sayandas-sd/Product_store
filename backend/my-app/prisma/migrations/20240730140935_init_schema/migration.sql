0bd447683226a1392e608d94a4caf8e8451fafc3
sayandas-sd <sayandas@Sayans-MacBook-Air.local>
2024-07-30 20:23:38 +0530

    added order product category item schema

---
+++ backend/my-app/prisma/migrations/20240730140935_init_schema/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "mobile" INTEGER NOT NULL,
+    "pincode" INTEGER NOT NULL,
+    "Address" TEXT NOT NULL,
+    "City" TEXT NOT NULL,
+    "State" TEXT NOT NULL,
+    "Landmark" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
